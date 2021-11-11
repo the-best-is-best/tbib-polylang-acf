4e43b5e0e19c4c1307b4b2aa0dd207edc51d8d24
the-best-is-best <michelle.raouf@outlook.com>
2021-11-11 12:47:00 +0200

    solve slug

--- tbib-polylang-acf.php
+++ tbib-polylang-acf.php
@@ -20,5 +20,5 @@ require_once ABSPATH . '/wp-content/plugins/tbib-update-plugin/tbib-update-check
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'https://raw.githubusercontent.com/the-best-is-best/tbib-polylang-acf/master/check.json',
 	__FILE__, //Full path to the main plugin file or functions.php.
-	'unique-plugin-or-theme-slug'
+	'tbib-polylang-acf'
 );
