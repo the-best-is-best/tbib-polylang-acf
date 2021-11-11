9058c5696a5a76f92890b7eae4bbf63e8008a22f
the-best-is-best <michelle.raouf@outlook.com>
2021-11-11 12:38:45 +0200

    now support auto update

--- tbib-polylang-acf.php
+++ tbib-polylang-acf.php
@@ -3,7 +3,7 @@
 
  * Plugin Name:       TBIB Polylang ACF
  * Description:       Adds multilingual capability to ACF With Polylang
- * Version:           1.1
+ * Version:           1.0
  * Requires at least: 5.4
  * Requires PHP:      5.6
  * Author:            Michelle Raouf - TBIB
@@ -15,3 +15,10 @@ if(!class_exists('Polylang')){
     return;
 }
 require_once 'src/acf/load.php';
+
+require_once ABSPATH . '/wp-content/plugins/tbib-update-plugin/tbib-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://raw.githubusercontent.com/the-best-is-best/tbib-polylang-acf/master/check.json',
+	__FILE__, //Full path to the main plugin file or functions.php.
+	'unique-plugin-or-theme-slug'
+);
